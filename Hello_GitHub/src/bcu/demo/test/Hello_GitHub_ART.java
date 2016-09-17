5c16ea93df10d09b1399f75e57cdaeb2b3fdbd02
eTutor <eTutor@ART>
2016-09-17 10:21:22 +0100

    Initial upload

---
+++ Hello_GitHub/src/bcu/demo/test/Hello_GitHub_ART.java
@@ -0,0 +1,18 @@
+package bcu.demo.test;
+
+public class Hello_GitHub_ART {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		displayGreeting();
+		MyData data = new MyData();
+		data.display();
+		
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Hello World");
+	}
+
+}
---
+++ Hello_GitHub/src/bcu/demo/test/MyData.java
@@ -0,0 +1,11 @@
+package bcu.demo.test;
+
+public class MyData {
+	int data[] = {0, 4, 42};
+
+	public void display(){
+		System.out.println("To Do : Implement data display");
+	
+	
+	}
+}
