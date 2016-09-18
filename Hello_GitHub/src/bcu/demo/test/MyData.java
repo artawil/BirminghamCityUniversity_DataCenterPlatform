8a01d604a19f9837d79145fe47e52551f972df7e
eTutor <eTutor@ART>
2016-09-18 08:43:30 +0100

    Added 87 to data

--- Hello_GitHub/src/bcu/demo/test/MyData.java
+++ Hello_GitHub/src/bcu/demo/test/MyData.java
@@ -1,11 +1,11 @@
 package bcu.demo.test;
 
 public class MyData {
-	int data[] = {0, 4, 42};
+	int data[] = {0, 4, 42, 87};
 
 	public void display(){
 		System.out.println("To Do : Implement data display");
 	
-	
+	  
 	}
 }
